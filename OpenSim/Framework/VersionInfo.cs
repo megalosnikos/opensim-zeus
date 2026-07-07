0e8bebd160906b00ebc9d450ff62f9fc0abeebe2
megalosnikos <nikosdvbsat@gmail.com>
2026-07-07 12:27:33 +0300

    Update VersionInfo.cs

--- OpenSim/Framework/VersionInfo.cs
+++ OpenSim/Framework/VersionInfo.cs
@@ -7,7 +7,7 @@
  *     * Redistributions of source code must retain the above copyright
  *       notice, this list of conditions and the following disclaimer.
  *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
+ *       notice, this list of conditions and the disclaimer in the
  *       documentation and/or other materials provided with the distribution.
  *     * Neither the name of the OpenSimulator Project nor the
  *       names of its contributors may be used to endorse or promote products
@@ -53,41 +53,12 @@ namespace OpenSim
 
         public static string GetVersionString(string versionNumber, Flavour flavour)
         {
-            string versionString = $"OpenSim {versionNumber} Nessie {flavour}";
+            string versionString = $"OpenSim {versionNumber} Zeus";
             return versionString.PadRight(VERSIONINFO_VERSION_LENGTH);
         }
 
         public const int VERSIONINFO_VERSION_LENGTH = 27;
 
-        /// <value>
-        /// This is the external interface version.  It is separate from the OpenSimulator project version.
-        ///
-        /// </value>
-        /// Commented because it's not used anymore, see below for new
-        /// versioning method.
-        //public readonly static int MajorInterfaceVersion = 8;
-
-        /// <summary>
-        /// This rules versioning regarding teleports, and compatibility between simulators in that regard.
-        /// </summary>
-        ///
-        /// <remarks>
-        /// The protocol version that we will use for outgoing transfers
-        /// Valid values are
-        /// "SIMULATION/0.8"
-        ///     - up to 45 avatar textures - 11 baked
-        /// "SIMULATION/0.7"
-        ///     TP uses call back again
-        /// "SIMULATION/0.3"
-        ///   - supports teleports to variable-sized regions
-        ///   - Older versions can teleport to this one, but only if the destination region
-        ///     is 256x256
-        /// "SIMULATION/0.2"
-        ///   - A source simulator which only implements "SIMULATION/0.1" can still teleport here
-        ///   - this protocol is more efficient than "SIMULATION/0.1"
-        /// "SIMULATION/0.1"
-        ///   - this is an older teleport protocol used in OpenSimulator 0.7.5 and before.
-        /// </remarks>
         public readonly static float SimulationServiceVersionAcceptedMin = 0.3f;
         public readonly static float SimulationServiceVersionAcceptedMax = 0.8f;
         public readonly static float SimulationServiceVersionSupportedMin = 0.3f;
